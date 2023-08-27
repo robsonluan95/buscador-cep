e6480b3e256ee018b8f3bbf79814b31e17fb1045
Robson Luan <rluan95@hotmail.com>
2023-08-27 20:58:38 -0300

    commit initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
+import { FiSearch } from 'react-icons/fi';
+import { useState } from 'react';
+import api  from './services';
 import './App.css';
 
 function App() {
+  const [buscaCep, setBuscaCep]=useState('')
+  const [cep,setCep]=useState({})
+
+  
+
+  async function buscarOCep(){
+    
+    if (buscaCep!==null){
+    }else{
+      alert('Digite algum CEP!')
+    }
+
+    try{
+      const response = await api.get(`${buscaCep}/json`)
+      setCep(response.data)
+      console.log(response.data)
+    }catch{
+      alert('erro ou CEP não encontrado')
+      setBuscaCep('')
+    }
+  }
+  function apertoEnter(e){
+    if (e.key==='Enter'){
+      buscarOCep()
+    }
+  }
+  
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className='titulo--buscador'>Busca CEP</h1>
+
+      <div className="input--botao">
+        <input
+        onKeyDown={ (e)=>{
+          apertoEnter(e)}}
+        id='aticaoBotao'
+        type='text'
+        placeholder="Digite seu Cep..."
+        value={buscaCep}
+        onChange={(evento)=> setBuscaCep(evento.target.value) }
+        />
+
+        <button id='botaoCep' onClick={buscarOCep} className="botao--buscador">
+          <FiSearch size={25} color='#FFF'/>
+        </button>
+
+      </div>  
+
+      {Object.keys(cep).length>0&&(
+        <div className='area--itens'>
+          {(cep.cep)!==''&&(
+            <h2>CEP: {cep.cep} </h2>
+          )}
+          
+          {(cep.logradouro)!==''&&(
+            <span>Rua: {cep.logradouro}</span>
+          )}
+          
+          {(cep.complemento)!==''&&(
+            <span>Complemento: {cep.complemento}</span>
+          )}
+
+          {(cep.bairro)!==''&&(
+            <span>Bairro:{cep.bairro}</span>
+          )}
+
+          {(cep.localidade)!==''&&(
+            <span>Cidade: {cep.localidade} - {cep.uf}</span>
+          )}
+          
+          
+          
+          
+          
+          
+        </div>
+      )}
+        
     </div>
   );
 }
